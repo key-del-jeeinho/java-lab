4fe9e37732a25d21faa3f90dd8bf7868092fb493
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-18 08:16:26 +0900

    [FIX] 학습테스트 결과를 주석을 통해 정리

--- src/test/groovy/com/velocia/javalab/반응형프로그래밍/WebFlux_학습_Mono와_Flux.groovy
+++ src/test/groovy/com/velocia/javalab/반응형프로그래밍/WebFlux_학습_Mono와_Flux.groovy
@@ -3,12 +3,21 @@ package com.velocia.javalab.반응형프로그래밍
 import reactor.core.publisher.Mono
 import spock.lang.Specification
 
-class Mono학습 extends Specification{
-    def "모노 만들어보기"() {
-        //모노는 여러번 block 이 가능하다.
-        //그 이유는 모노 자체가 변하지 않아서 인듯하다 (불변객체인지 테스트해보자).
-        //모노.map 을 할 경우 새로운 Mono 를 생성해서 반환하는 것 같다.
-        //모노 안에 값이 없으면 null 을 반환한다.
+class WebFlux_학습_Mono와_Flux extends Specification{
+    def "모노로 놀아보기 (스트림에서 작동하던 함수들 동작시켜보기)"() {
+        /**
+         모노 안에 값이 없으면 null 을 반환한다.
+
+         모노는 여러번 block 이 가능하다. 즉 모노는 여러번 재사용이 가능하다.
+         -> 이에 대한 정확한 문서를 찾아 보던 중, 다음과 같은 문구를 발견하였다
+         -> Project Reactor streams are also reusable, which is a significant difference from Java 8 Streams.
+         ->즉, Stream 과 달리 모노는 여러번 재사용이 가능하다.
+
+         MonoJust 를 뜯어보니까 불변객체인듯하다 (value 가 final 객체)
+         ->다만, deepcopy 를 하는지는 모르니 완전히 불변객체라 하긴 힘들지도
+
+         여기서 생긴 궁금증, 모노.filter/map 같은 함수를 사용할 경우 새로운 모노 객체를 생성하는걸까?
+         */
         given:
         Long 모노안에_들어갈_데이터 = 231
         Mono<Long> 모노 = Mono<Long>.just(모노안에_들어갈_데이터 as Long)
